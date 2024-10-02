d4296fe144a0aebac3623ed91537fe97c5a7b3e4
MelikaEl <melika.melika8@gmail.com>
2024-10-02 11:56:33 +0330

    add list functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 import {FaPlus, FaPencilAlt, FaTrash} from 'react-icons/fa';
 
 
 
 function App() {
+
+  const [todos, setTodos]=useState([{id:1,todo:'Learn React'}]);
+  const [input,setInput]=useState('');
+
+  const addTodo=async()=>{
+    try{
+      if(input.trim() !== ''){
+        setTodos([...todos,{id:new Date(),todo:input}]);
+        setInput('')
+      }
+    } catch (error){
+      console.error(error.message);
+    }
+  }
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-4 p-4 bg-custom-background bg-center bg-cover">
       <div className="bg-gray-100 p-6 rounded shadow-md w-full max-w-lg lg:w-1/4">
@@ -13,34 +29,34 @@ function App() {
             type="text"
             placeholder="Add a todo"
             className="py-2 px-4 border rounded w-full focus:outline-none mr-2"
+            value={input}
+            onChange={(e)=>setInput(e.target.value)}
           />
-          <button className="bg-gradient-to-r from-blue-400 to-blue-600 text-white py-2 px-4 rounded">
+          <button onClick={addTodo} className="bg-gradient-to-r from-blue-400 to-blue-600 text-white py-2 px-4 rounded">
             <FaPlus/>
           </button>
         </div>
       </div>
 
-      <div className="bg-gray-100 p-6 rounded shadow-md w-full max-w-lg lg:w-1/4">
-        <ul>
-          <li className="flex items-center justify-between bg-white p-3 rounded shadow-md mb-3">
-            <span className="text-lg">Learn React</span>
-            <div>
-              <button className="mr-2 p-2 bg-gradient-to-r from-gray-400 to-gray-600 text-white rounded hover:from-gray-500 hover:to-gray-700 "><FaPencilAlt/></button>
-              <button className="p-2 bg-gradient-to-r from-red-400 to-red-600 text-white rounded hover:from-red-500 hover:to-red-700 "><FaTrash/></button>
-            </div>
-          </li>
-          <li className="flex items-center justify-between bg-white p-3 rounded shadow-md mb-3">
-            <span className="text-lg">Learn React</span>
-            <div>
-              <button className="mr-2 p-2 bg-gradient-to-r from-gray-400 to-gray-600 text-white rounded hover:from-gray-500 hover:to-gray-700 "><FaPencilAlt/></button>
-              <button className="p-2 bg-gradient-to-r from-red-400 to-red-600 text-white rounded hover:from-red-500 hover:to-red-700 "><FaTrash/></button>
-            </div>
-          </li>
+      {
+        todos.length > 0 && (
+          <div className="bg-gray-100 p-6 rounded shadow-md w-full max-w-lg lg:w-1/4">
+          <ul>
+            {todos.map((todo,index)=>(
+            <li key={index} className="flex items-center justify-between bg-white p-3 rounded shadow-md mb-3">
+              <span className="text-lg">{todo.todo}</span>
+              <div>
+                <button onClick={()=>setEdit(index)} className="mr-2 p-2 bg-gradient-to-r from-gray-400 to-gray-600 text-white rounded hover:from-gray-500 hover:to-gray-700 "><FaPencilAlt/></button>
+                <button className="p-2 bg-gradient-to-r from-red-400 to-red-600 text-white rounded hover:from-red-500 hover:to-red-700 "><FaTrash/></button>
+              </div>
+            </li>
+            ))}
+          </ul>
+        </div>
+        )
+      }
 
-          
-          
-        </ul>
-      </div>
+      
     </div>
   );
 }
